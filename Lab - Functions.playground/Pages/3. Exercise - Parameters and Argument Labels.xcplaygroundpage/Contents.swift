584dee31a4ee068bc9072af17fb4ea597b99ccb0
Jackson <JacksonCokeNJ@gmail.com>
2023-09-28 13:57:47 -0400

    .

--- Lab - Functions.playground/Pages/3. Exercise - Parameters and Argument Labels.xcplaygroundpage/Contents.swift
+++ Lab - Functions.playground/Pages/3. Exercise - Parameters and Argument Labels.xcplaygroundpage/Contents.swift
@@ -4,9 +4,9 @@
  Write a new introduction function called `introduction`. It should take two `String` parameters, `name` and `home`, and one `Int` parameter, `age`. The function should print a brief introduction. I.e. if "Mary," "California," and 32 were passed into the function, it might print "Mary, 32, is from California." Call the function and observe the printout.
  */
 func introduction(_ Name: String, Loc: String, Age: Int){
-    print(+Name+", "+String(Age)+", is from "+Loc)
+    print(Name+", "+String(Age)+", is from "+Loc)
 }
-introduction(Name: "Marc", Loc: "Indo", Age: 17)
+
 
 //:  Write a function called `almostAddition` that takes two `Int` arguments. The first argument should not require an argument label. The function should add the two arguments together, subtract 2, then print the result. Call the function and observe the printout.
 func almostAddition(_ argument1 : Int, argument2 : Int){
